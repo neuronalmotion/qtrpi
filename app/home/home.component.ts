e1f658a154e221ea214c32d76dbb74aa684d777d
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-07 17:38:22 +0200

    Add home sections

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: "home",
-	template: `<h1>{{title}}</h1>`
+	templateUrl: 'home.component.html'
 })
 
 export class HomeComponent {
-	title = 'Home sweet home';
 }
