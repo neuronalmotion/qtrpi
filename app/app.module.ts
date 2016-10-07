904336d9dd9728657842b826d5593efd3fe9c77b
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-07 16:50:09 +0200

    Add navbar with Bootstrap

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
-import { HomeComponent } from './home.component';
-import { DownloadComponent } from './download.component';
+import { AppComponent }  from './app/app.component';
+import { HomeComponent } from './home/home.component';
+import { DownloadComponent } from './download/download.component';
 
 @NgModule({
   imports: [
---
+++ app/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: "app",
+	templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+	title = 'Qt Raspberry Pi binaries';
+}
