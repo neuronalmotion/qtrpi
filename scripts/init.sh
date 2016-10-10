4381b745fddd3caa35d9972f4e0fab6f15fe6789
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-10 16:59:45 +0200

    Only copy useful folders to sysroot

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,22 +6,32 @@ pushd raspi
 git clone https://github.com/raspberrypi/tools
 popd
 
-# Download and unzip the latest wheezy image
+# Download and unzip the latest wheezy image (~1.4Go zipped)
 mkdir raspbian
 pushd raspbian 
 wget --content-disposition https://downloads.raspberrypi.org/raspbian_latest
 unzip *raspbian*.zip
 
-# Mount and extract sysroot 
+# Mount and extract the raspbian sysroot 
 sudo losetup -P /dev/loop0 *raspbian*.img
 sudo mount /dev/loop0p2 /mnt
 mkdir sysroot
-sudo rsync -a /mnt/ sysroot/
+
+# We do not really all + root permission problem
+#sudo rsync -a /mnt/ sysroot/
+
+# Cherry-pick copy
+mkdir -p sysroot/lib sysroot/opt sysroot/usr/include sysroot/usr/lib
+cp -r /mnt/lib/ sysroot/
+cp -r /mnt/usr/include sysroot/usr/
+cp -r /mnt/usr/lib sysroot/usr/
+cp -r /mnt/opt/vc sysroot/opt/
+
 sudo umount /mnt
 sudo losetup -d /dev/loop0
 popd
 
-# Retrieve qtbase source code
+# Retrieve qtbase source code (~440 Mo)
 mkdir modules
 pushd modules
 git clone git://code.qt.io/qt/qtbase.git -b 5.7
