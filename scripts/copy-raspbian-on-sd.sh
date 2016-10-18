e75195304464149b2ec15477139b2e863ad2a047
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-18 20:29:24 +0200

    Add script to copy raspbian on the SD card

---
+++ scripts/copy-raspbian-on-sd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMG=$(ls raspbian/*raspbian*.img)
+TARGET=/dev/sdd
+
+echo "Copying $IMG to $TARGET..."
+
+# Copy raspbian on the target
+sudo dd bs=4M if=$IMG of=$TARGET status=progress
+
+# Ensure the cache is flushed
+sudo sync
+
+echo "Done! You can eject the SD card."
