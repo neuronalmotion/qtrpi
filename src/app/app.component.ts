238f63433bad16f12904068d443ac734b99734e9
Robin Penea <robin.penea@gmail.com>
2016-10-21 18:25:12 +0200

    Add Google Analytics
    Close #21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router }   from '@angular/router';
+
+import "rxjs/add/operator/distinctUntilChanged"
+
+// Declare ga function as ambient
+declare var ga: any;
 
 @Component({
-  selector: 'app',
-	templateUrl: './app.component.html'
+    selector: 'app',
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent {
-	title = 'Qt Raspberry Pi binaries';
+    constructor(public router: Router) {
+        router.events.distinctUntilChanged((previous: any, current: any) => {
+            if(current instanceof NavigationEnd) {
+                return previous.url === current.url;
+            }
+            return true;
+        }).subscribe((x: any) => {
+            console.log('router.change', x);
+            ga('send', 'pageview', x.url);
+        });
+    }
 }
 
