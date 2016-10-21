ad44b3dec2e650f8aeb66ab54987c9dbd79103a5
Robin Penea <robin.penea@gmail.com>
2016-10-21 17:50:08 +0200

    Add banner in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'home',
-	templateUrl: './home.component.html'
+	templateUrl: './home.component.html',
+    styleUrls: [ './home.component.css' ]
 })
 
 export class HomeComponent {
