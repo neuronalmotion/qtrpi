afe66788fd7beca3a4ad8bb1ab4e702133403aad
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-10-21 18:36:54 +0200

    WIP Add a script to prepare the sysroot with qemu

---
+++ scripts/prepare-sysroot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get install qemu-user-static
+sudo cp /usr/bin/qemu-arm-static sysroot/usr/bin
+
+# Mount sysroot part
+sudo mount -o bind /proc sysroot/proc
+sudo mount -o bind /dev sysroot/dev
+sudo mount -o bind /sys sysroot/sys
+
+# TODO: comment line in file sysroot/etc/ld.so.preload
+
+# TODO: remove comment on deb-src on file /etc/apt/sources.list
+
+# Enter the Matrix
+sudo chroot sysroot
+
+# Global update
+apt-get update
+apt-get dist-upgrade
+# Care with changelog and chromium waiting a key pres...
+
+# Install Qt depnedencies
+apt-get install apt-transport-https
+apt-get build-dep qt4-x11
+apt-get build-dep qtbase-opensource-src
+apt-get install libudev-dev libinput-dev libts-dev libxcb-xinerama0-dev libxcb-xinerama0
+
+exit
+sudo umount sysroot/sys
+sudo umount sysroot/dev
+sudo umount sysroot/proc
+
+
+sudo chown -R guillaume:guillaume sysroot
