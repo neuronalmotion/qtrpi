8d5a02ec049d9e93b591359ef1a6b593b8d92f2c
Robin Penea <robin.penea@gmail.com>
2016-10-31 16:03:57 +0100

    Fix compilation of qopenglwidget example
    Close #29

--- utils/compile-example-qopenglwidget.sh
+++ utils/compile-example-qopenglwidget.sh
@@ -6,13 +6,12 @@ cd_root
 # Crosscompile qtbase
 CURRENT_DIR=$(pwd)
 export CROSS_COMPILE=$ROOT/raspi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-
-export DIRROOT=$ROOT
 export SYSROOT=$ROOT/raspbian/sysroot
 
 cd modules/qtbase/examples/opengl/qopenglwidget
 
 # Generate the Makefile with qmake (from the host)
-$CURRENT_DIR/raspi/qt5/bin/qmake
+$ROOT/bin/qmake-qtrpi
 
 # Build the host tools
 make -j 10
