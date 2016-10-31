accbd65e36b53cdd298aedd59d8a216c8c2c8f4c
Robin Penea <robin.penea@gmail.com>
2016-10-31 14:58:01 +0100

    Remove date from compressed qtrpi files

--- utils/generate-compressed-qtrpi.sh
+++ utils/generate-compressed-qtrpi.sh
@@ -7,7 +7,5 @@ cd_root
 BASE_DIR=dist/qtrpi/$DEVICE_NAME
 mkdir -p $BASE_DIR
 
-DATE=$(date +%Y%m%d)
-
 message 'Compressing qtrpi...'
-zip -r -q $ROOT/$BASE_DIR/qtrpi-${DEVICE_NAME}_qt-${QT_VERSION}_${DATE}.zip raspi/qt5 raspi/qt5pi
+zip -r -q $ROOT/$BASE_DIR/qtrpi-${DEVICE_NAME}_qt-${QT_VERSION}.zip raspi/qt5 raspi/qt5pi
