dc6c2ece8a7aa8d53f817a59227faa113b1ec1f5
Robin Penea <robin.penea@gmail.com>
2016-10-31 14:21:50 +0100

    Fix nested directories in sysroot-minimal
    Close #23

--- utils/prepare-sysroot-minimal.sh
+++ utils/prepare-sysroot-minimal.sh
@@ -6,8 +6,8 @@ cd_root
 SYSROOT_FULL_DIR=$ROOT/raspbian/sysroot-full
 SYSROOT_MINIMAL_DIR=$ROOT/raspbian/sysroot-minimal
 
-for DIR in lib 'usr/include' 'usr/lib' 'opt/vc'; do
+for DIR in 'lib' 'usr/include' 'usr/lib' 'opt/vc'; do
     mkdir -p $SYSROOT_MINIMAL_DIR/$DIR
-    rsync -av $SYSROOT_FULL_DIR/$DIR $SYSROOT_MINIMAL_DIR/$DIR
+    rsync -av $SYSROOT_FULL_DIR/$DIR/ $SYSROOT_MINIMAL_DIR/$DIR
 done
 
