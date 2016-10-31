8d5a7c7e8a251a142996ac6741cce795afc9b852
Robin Penea <robin.penea@gmail.com>
2016-10-31 09:17:54 +0100

    Add date suffix to qt binaries file
    Close #24

--- utils/generate-compressed-qtrpi.sh
+++ utils/generate-compressed-qtrpi.sh
@@ -5,6 +5,7 @@ source ${0%/*}/common.sh
 cd_root
 
 mkdir dist
+DATE=$(date +%Y%m%d)
 
 message 'Compressing qtrpi...'
-zip -r -q $ROOT/dist/qtrpi-$TARGET_DEVICE-qt-$QT_VERSION.zip raspi/qt5 raspi/qt5pi
+zip -r -q $ROOT/dist/qtrpi-${TARGET_DEVICE}_qt-${QT_VERSION}_${DATE}.zip raspi/qt5 raspi/qt5pi
