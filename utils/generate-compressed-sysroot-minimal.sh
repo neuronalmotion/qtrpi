5d4d61045104b62fdc068de8260abf3a0ea5fc32
Robin Penea <robin.penea@gmail.com>
2016-10-31 12:30:58 +0100

    Remove missing typo

--- utils/generate-compressed-sysroot-minimal.sh
+++ utils/generate-compressed-sysroot-minimal.sh
@@ -10,7 +10,6 @@ mkdir -p $BASE_DIR
 IMG=$(ls raspbian/*raspbian*.img)
 FILENAME=$(basename "$IMG")
 BASENAME=${FILENAME%%.*}
-FILE_DST=
 
 message 'Compressing sysroot-minimal...'
 zip -r -q $ROOT/$BASE_DIR/qtrpi-sysroot-minimal-$BASENAME.zip raspbian/sysroot-minimal
