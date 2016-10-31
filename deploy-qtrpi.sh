0a0933bbbac535d192bd3dd43856832497a076cb
Robin Penea <robin.penea@gmail.com>
2016-10-31 15:29:57 +0100

    Update QTRPI_HOST to QTRPI_TARGET_HOST
    Close #28

--- deploy-qtrpi.sh
+++ deploy-qtrpi.sh
@@ -5,10 +5,10 @@ OUTPUT_DIR=$ROOT/raspi/qt5pi
 
 cd_root
 
-ssh $RPI_HOST 'sudo mkdir /usr/local/qt5pi ; sudo chown -R pi:pi /usr/local/qt5pi'
-ssh $RPI_HOST 'sudo apt-get install -y libts-0.0-0 libinput5'
-ssh $RPI_HOST "sudo sh -c 'echo /usr/local/qt5pi/lib > /etc/ld.so.conf.d/qt5pi.conf'"
+ssh $TARGET_HOST 'sudo mkdir /usr/local/qt5pi ; sudo chown -R pi:pi /usr/local/qt5pi'
+ssh $TARGET_HOST 'sudo apt-get install -y libts-0.0-0 libinput5'
+ssh $TARGET_HOST "sudo sh -c 'echo /usr/local/qt5pi/lib > /etc/ld.so.conf.d/qt5pi.conf'"
 
-rsync -avz $OUTPUT_DIR $RPI_HOST:/usr/local/
+rsync -avz $OUTPUT_DIR $TARGET_HOST:/usr/local/
 
-ssh $RPI_HOST 'sudo ldconfig'
+ssh $TARGET_HOST 'sudo ldconfig'
