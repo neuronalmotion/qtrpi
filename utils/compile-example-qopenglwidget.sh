d7048c20ef5ee32d8f64ab52783f3bd66cfac0a1
Robin Penea <robin.penea@gmail.com>
2016-11-02 17:28:36 +0100

    Remove uneeded exports in compile-example-qopenglwidget.sh

--- utils/compile-example-qopenglwidget.sh
+++ utils/compile-example-qopenglwidget.sh
@@ -5,9 +5,6 @@ cd_root
 
 cd modules/qtbase/examples/opengl/qopenglwidget
 
-export CROSS_COMPILE=$ROOT/raspi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-
-export SYSROOT=$ROOT/raspbian/sysroot
-
 $ROOT/bin/qmake-qtrpi
 
 make -j 10
