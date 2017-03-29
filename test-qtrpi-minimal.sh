d63719cc4a51bb5cec4a3cf67acf011ab7e79cc2
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2017-03-29 11:58:54 +0200

    Fix common init for the test script

--- test-qtrpi-minimal.sh
+++ test-qtrpi-minimal.sh
@@ -2,6 +2,8 @@
 
 source ${0%/*}/utils/common.sh
 
+$UTILS_DIR/init-common.sh
+
 export QTRPI_CURL_OPT="--user $CI_AUTH_USER:$CI_AUTH_PASSWORD"
 export QTRPI_BASE_URL="$CI_BASE_URL/job/qtrpi/QT_VERSION=$QTRPI_QT_VERSION,TARGET_DEVICE=$QTRPI_TARGET_DEVICE/lastSuccessfulBuild/artifact/dist"
 
