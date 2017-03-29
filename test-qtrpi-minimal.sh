9ba772517a196f8994064c53da9a8f0fcd560d96
Robin Penea <robin.penea@gmail.com>
2017-03-29 15:18:11 +0200

    Fix qtrpi minimal url in test script

--- test-qtrpi-minimal.sh
+++ test-qtrpi-minimal.sh
@@ -4,8 +4,9 @@ source ${0%/*}/utils/common.sh
 
 $UTILS_DIR/init-common.sh
 
-export QTRPI_CURL_OPT="--user $CI_AUTH_USER:$CI_AUTH_PASSWORD"
-export QTRPI_BASE_URL="$CI_BASE_URL/job/qtrpi/QT_VERSION=$QTRPI_QT_VERSION,TARGET_DEVICE=$QTRPI_TARGET_DEVICE/lastSuccessfulBuild/artifact/dist"
+QTRPI_CURL_OPT="--user $CI_AUTH_USER:$CI_AUTH_PASSWORD"
+QTRPI_BASE_URL="$CI_BASE_URL/job/qtrpi/QT_VERSION=$QTRPI_QT_VERSION,TARGET_DEVICE=$QTRPI_TARGET_DEVICE/lastSuccessfulBuild/artifact/dist"
+QTRPI_MINIMAL_URL="$QTRPI_BASE_URL/qtrpi/$DEVICE_NAME/$QTRPI_ZIP"
 
 pushd $ROOT
 
