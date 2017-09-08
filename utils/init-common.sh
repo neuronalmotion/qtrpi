611334d19ca5d42917fa4fa57f75fe42d76fe983
Alexander Rose <arose@haprotec.de>
2017-09-08 07:52:57 -0700

    init-common modified for use with docker build

--- utils/init-common.sh
+++ utils/init-common.sh
@@ -4,15 +4,23 @@ source ${0%/*}/common.sh
 
 message 'Creating qtrpi directory structure'
 
-# sanity check for mounted image
-sudo umount /mnt/raspbian
-sudo umount $ROOT/raspbian/sysroot-full/sys
-sudo umount $ROOT/raspbian/sysroot-full/dev
-sudo umount $ROOT/raspbian/sysroot-full/proc
+if [[ ! $QTRPI_DOCKER ]]; then
+    # sanity check for mounted image
+    sudo umount /mnt/raspbian
+    sudo umount $ROOT/raspbian/sysroot-full/sys
+    sudo umount $ROOT/raspbian/sysroot-full/dev
+    sudo umount $ROOT/raspbian/sysroot-full/proc
+    
+    sudo mkdir -p $ROOT
+    sudo chown -R $USER:$USER $ROOT
+else
+    # === Docker-Mode ===
+    # umount is not allowed in a docker image, besides we use a fresh linux image => skipping sanity checks
+    # sudo is also not needed/installed by default => creating directory structure without use of sudo
+    mkdir -p $ROOT
+    chown -R $USER:$USER $ROOT
+fi
 
-sudo mkdir -p $ROOT
-sudo chown -R $USER:$USER $ROOT
 cd_root
 
 mkdir raspi raspbian bin logs
-
